914b7f0cc1669c7e92bded9dc816e540f1629c77
Fabio Rehm <fgrehm@gmail.com>
2012-08-16 10:30:09 -0300

    Fix download paths

--- etc/rbenv.d/install/install-remote.bash
+++ etc/rbenv.d/install/install-remote.bash
@@ -10,14 +10,14 @@ if [ -z "$RBENV_ROOT" ]; then
   RBENV_ROOT="${HOME}/.rbenv"
 fi
 
-DOWNLOAD_PATH="$RBENV_ROOT/plugins/ruby-build-remote/share/ruby-build-remote"
+DOWNLOAD_PATH="$RBENV_ROOT/plugins/rbenv-install-remote/share/ruby-build-remote"
 mkdir -p $DOWNLOAD_PATH
 
 URL=${ARGUMENTS[0]}
 VERSION_NAME=${URL##*/}
 VERSION_NAME=${VERSION_NAME%%\?*}
 
-[[ -f $DOWNLOAD_PATH/$2 ]] && { echo "There is already a definition for $2"; exit 1; }
-wget $1 -O $DOWNLOAD_PATH/$2
+[[ -f $DOWNLOAD_PATH/$VERSION_NAME ]] && { echo "There is already a definition for $VERSION_NAME"; exit 1; }
+wget $1 -O $DOWNLOAD_PATH/$VERSION_NAME
 
 export DEFINITION=$DOWNLOAD_PATH/$VERSION_NAME
